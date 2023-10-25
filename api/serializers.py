83255145e018bb9bea881350112b2af24939b909
Anas <anascoder4@gmail.com>
2023-10-25 19:39:01 +0530

    Dish by ingredients

--- api/serializers.py
+++ api/serializers.py
@@ -2,47 +2,66 @@ from .models import *
 from users.models import ChefUser
 from rest_framework import serializers
 
-class DishInstructionSerializer(serializers.ModelSerializer):
+
+class InstructionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DishInstruction
-        fields = "__all__"
+        model = Instructions
+        fields = ('step',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'quantity')
+
 
 class DishSerializer(serializers.ModelSerializer):
-    # instructions = DishInstructionSerializer(many=True, read_only=True)
+    ingredients = IngredientSerializer(many=True)
+    instructions = InstructionSerializer(many=True)
+
     class Meta:
         model = Dish
         fields = (
-            'name', 'instructions', 'chef', 'veg_non_veg', 'popularity_state',
-            'cuisine', 'main_course_starter_dessert', 'quantity', 'customizable_ingredients',
-            'cooking_time', 'dish_picture'
-        )   
-    # def create(self, validated_data):
-    #     instructions_data = validated_data.pop('instructions', []) 
-    #     dish = Dish.objects.create(**validated_data)
-    #     for instruction_data in instructions_data:
-    #         DishInstruction.objects.create(**instruction_data)
-    #     dish.instruction = DishInstruction
-    #     dish.save()
-    #     return dish
+            'name', 'ingredients', 'instructions', 'chef', 'veg_non_veg', 'popularity_state',
+            'cuisine', 'main_course_starter_dessert', 'customizable_ingredients',
+            'cooking_time', 'dish_picture', 'dish_video_url'
+        )
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        instructions_data = validated_data.pop('instructions')
+        dish = Dish.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:
+            Ingredient.objects.create(dish=dish, **ingredient_data)
+        for instruction_data in instructions_data:
+            Instructions.objects.create(dish=dish, **instruction_data)
+        return dish
+
 
 class DishMinimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
-        fields = ['id','name']
+        fields = ['id', 'name']
+
 
 class ChefSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChefUser
         fields = ['id', 'first_name', 'last_name', 'email', 'profile_picture']
 
+
 class ChefListSerializer(serializers.ModelSerializer):
     dishes = serializers.SerializerMethodField()
+
     class Meta:
         model = ChefUser
-        fields = ['id', 'first_name', 'last_name', 'email', 'profile_picture', 'dishes']
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'profile_picture', 'dishes']
+
     def get_dishes(self, obj):
         dishes = Dish.objects.filter(chef=obj)
         return DishMinimalSerializer(dishes, many=True).data
+
 
 class ContactSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
